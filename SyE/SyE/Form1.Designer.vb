0357a987828d27e9351505b713537b712b2be6fe
Ashura8 <162086522+Ashura8@users.noreply.github.com>
2024-03-03 23:17:23 -0600

    Add files via upload

---
+++ SyE/SyE/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(84, 244)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(64, 64)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(83, 311)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Lanzar "
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(173, 244)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(64, 64)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 22)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(301, 205)
+        Me.TableLayoutPanel1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(126, 312)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "J1 "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(215, 312)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "J2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(173, 311)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Lanzar "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(328, 92)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 9
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(329, 72)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "J1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Blue
+        Me.Label6.Location = New System.Drawing.Point(329, 123)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(20, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "J2"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(328, 143)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox2.TabIndex = 11
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(323, 22)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(142, 29)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Posiciones"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(475, 339)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Serpientes y Escaleras"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label7 As Label
+End Class
---
+++ SyE/SyE/Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+    Dim random As New Random()
+    Dim J1 As Integer = 0
+    Dim J2 As Integer = 0
+    Dim currentPlayer As Integer = 1
+    Dim A As Integer
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        Dim av As Integer = random.Next(1, 7)
+        J1 += av
+        If J1 > 72 Then J1 = 72
+        J1 = comodines(J1) ' Llama a la función comodines para actualizar la posición
+        Pocision(J1, Color.Red)
+        MsgBox("Avanza " & av)
+        MsgBox("Termino el turno del Jugador 1")
+
+        If J1 >= 72 And J2 >= 72 Then
+            MessageBox.Show("Empate")
+            PictureBox2.Enabled = False
+            PictureBox1.Enabled = False
+        ElseIf J1 >= 72 Then
+            MessageBox.Show("¡Jugador 1 gana!")
+            PictureBox2.Enabled = False
+            PictureBox1.Enabled = False
+        End If
+        PictureBox1.Enabled = False
+        PictureBox2.Enabled = True
+        TextBox1.Text = "Esta en la casilla " & J1.ToString()
+        'Dim av As Integer = random.Next(1, 7)
+        'J1 += av
+        'If J1 > 72 Then J1 = 72
+        'Pocision(J1, Color.Red)
+        'MsgBox("Avanza " & av)
+        'MsgBox("Termino el turno del Jugador 1")
+
+        'If J1 >= 72 And J2 >= 72 Then
+        '    MessageBox.Show("Empate")
+        '    PictureBox2.Enabled = False
+        '    PictureBox1.Enabled = False
+        'ElseIf J1 >= 72 Then
+        '    MessageBox.Show("¡Jugador 1 gana!")
+        '    PictureBox2.Enabled = False
+        '    PictureBox1.Enabled = False
+        'End If
+        'PictureBox1.Enabled = False
+        'PictureBox2.Enabled = True
+        'TextBox1.Text = "Esta en la casilla " & J1.ToString()
+    End Sub
+    Sub llenar()
+        TableLayoutPanel1.ColumnCount = 10
+        TableLayoutPanel1.RowCount = 7
+        TableLayoutPanel1.ColumnStyles.Clear()
+        TableLayoutPanel1.RowStyles.Clear()
+        For i As Integer = 0 To 9
+            TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 10))
+        Next
+        For i As Integer = 0 To 6
+            TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 14.3))
+        Next
+
+        For i As Integer = 0 To 71
+            Dim tab As New Label()
+            tab.BorderStyle = BorderStyle.FixedSingle
+            tab.Dock = DockStyle.Fill
+            tab.TextAlign = ContentAlignment.MiddleCenter
+            tab.Text = (i + 1).ToString()
+            TableLayoutPanel1.Controls.Add(tab)
+        Next
+        ' Marcar las posiciones iniciales de los jugadores
+        Pocision(J2, Color.Blue)
+        Pocision(J1, Color.Red)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenar()
+        PictureBox2.Enabled = False
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+    End Sub
+
+    Function comodines(pos As Integer)
+
+        Dim nPos As Integer = pos
+
+        Select Case pos
+            Case 59
+                nPos = 70
+                MsgBox("Escalera: Avanza a la casilla 70")
+            Case 22
+                nPos = 3
+                MsgBox("Serpiente: Regresa a la casilla 3")
+            Case 61
+                nPos = 72
+                MsgBox("Escalera: Avanza a la casilla 72")
+            Case 12
+                nPos = 50
+                MsgBox("Escalera: Avanza a la casilla 50")
+            Case 19
+                nPos = 1
+                MsgBox("Serpiente: Regresa a la casilla 1")
+            Case 33
+                nPos = 21
+                MsgBox("Serpiente: Regresa a la casilla 21")
+            Case 71
+                nPos = 1
+                MsgBox("Serpiente: Regresa a la casilla 1")
+        End Select
+
+        Return nPos
+    End Function
+    Private Sub Pocision(pos As Integer, color As Color)
+        pos = comodines(pos)
+
+        Dim tabIndex As Integer = pos - 1
+        For Each control As Control In TableLayoutPanel1.Controls
+            control.BackColor = Color.White
+        Next
+
+        If tabIndex >= 0 AndAlso tabIndex < TableLayoutPanel1.Controls.Count Then
+            If color = Color.Red Then
+                TableLayoutPanel1.Controls(tabIndex).BackColor = Color.Red
+            ElseIf color = Color.Blue Then
+                TableLayoutPanel1.Controls(tabIndex).BackColor = Color.Blue
+            End If
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Dim av As Integer = random.Next(1, 7)
+        J2 += av
+        If J2 > 72 Then J2 = 72
+        J2 = comodines(J2) ' Llama a la función comodines para actualizar la posición
+        Pocision(J2, Color.Blue)
+        MsgBox("Avanza " & av)
+        MsgBox("Termino el turno del Jugador 2")
+
+        If J1 >= 72 And J2 >= 72 Then
+            MessageBox.Show("Empate")
+            PictureBox2.Enabled = False
+            PictureBox1.Enabled = False
+        ElseIf J2 >= 72 Then
+            MessageBox.Show("¡Jugador 2 gana!")
+            PictureBox2.Enabled = False
+            PictureBox1.Enabled = False
+        End If
+        PictureBox2.Enabled = False
+        PictureBox1.Enabled = True
+        TextBox2.Text = "Esta en la casilla " & J2.ToString()
+    End Sub
+
+
+End Class
